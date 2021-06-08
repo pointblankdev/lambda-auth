cb4fbcebf9bfb0fb5455e76b96bbb8e2c9e244ef
Ross Ragsdale <ross.ragsdale@gmail.com>
2021-06-07 21:14:24 -0500

    :children_crossing: Simplify API interface.

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
 const jwksClient = require("jwks-rsa");
 const jwt = require("jsonwebtoken");
 
-class LambdaAuth {
-  client;
-  constructor(url) {
-    this.client = jwksClient({
-      jwksUri: "https://unitedingaming.us.auth0.com/.well-known/jwks.json" || url,
-    });
-  }
+const client = jwksClient({
+  jwksUri: "https://unitedingaming.us.auth0.com/.well-known/jwks.json",
+});
 
-  useJwks = (token) =>
-    new Promise((resolve, reject) => {
-      jwt.verify(
-        token,
-        (header, callback) => {
-          this.client.getSigningKey(header.kid, (err, key) => {
-            const signingKey = key.publicKey || key.rsaPublicKey;
-            callback(null, signingKey);
-          });
-        },
-        null,
-        (err, resp) => resolve(resp)
-      );
-    });
-}
+const verify = (token) =>
+  new Promise((resolve, reject) => {
+    jwt.verify(
+      token,
+      (header, callback) => {
+        client.getSigningKey(header.kid, (err, key) => {
+          const signingKey = key.publicKey || key.rsaPublicKey;
+          callback(null, signingKey);
+        });
+      },
+      null,
+      (err, resp) => resolve(resp)
+    );
+  });
 
-module.exports = LambdaAuth;
+module.exports = { verify };
