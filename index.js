7d278a379fec02657b7fd825ff83d32539abb408
Ross Ragsdale <ross.ragsdale@gmail.com>
2021-07-28 17:39:05 -0500

    :art: Try catch for now.

--- index.js
+++ index.js
@@ -7,30 +7,34 @@ const client = jwksClient({
 });
 
 const verify = (req) => {
-  console.log(req);
-  const [bearer, token] = get(req, "headers.Authorization").split(" ");
-  if (bearer !== "Bearer") throw Error("Bearer prefix missing.");
-  if (!token) throw Error("Token undefined.");
-  return new Promise((resolve, reject) => {
-    jwt.verify(
-      token,
-      (header, callback) => {
-        client.getSigningKey(header.kid, (err, key) => {
-          const signingKey = key.publicKey || key.rsaPublicKey;
-          callback(null, signingKey);
-        });
-      },
-      null,
-      (err, resp) => {
-        if (err) {
-          console.error(err);
-          resolve({});
-        } else {
-          resolve(resp);
+  try {
+    console.log(req.headers);
+    const [bearer, token] = get(req, "headers.Authorization").split(" ");
+    if (bearer !== "Bearer") throw Error("Bearer prefix missing.");
+    if (!token) throw Error("Token undefined.");
+    return new Promise((resolve, reject) => {
+      jwt.verify(
+        token,
+        (header, callback) => {
+          client.getSigningKey(header.kid, (err, key) => {
+            const signingKey = key.publicKey || key.rsaPublicKey;
+            callback(null, signingKey);
+          });
+        },
+        null,
+        (err, resp) => {
+          if (err) {
+            console.error(err);
+            resolve({});
+          } else {
+            resolve(resp);
+          }
         }
-      }
-    );
-  });
+      );
+    });
+  } catch (error) {
+    return {};
+  }
 };
 
 module.exports = { verify };
