cce4bccb1f8639209ff2a0ed7491842de88c669d
Ross Ragsdale <ross.ragsdale@gmail.com>
2021-07-28 17:31:08 -0500

    :bug: Fix case-sensitive bug.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const client = jwksClient({
 
 const verify = (req) => {
   console.log(req);
-  const [bearer, token] = get(req, "headers.authorization").split(" ");
+  const [bearer, token] = get(req, "headers.Authorization").split(" ");
   if (bearer !== "Bearer") throw Error("Bearer prefix missing.");
   if (!token) throw Error("Token undefined.");
   return new Promise((resolve, reject) => {
